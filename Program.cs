92fabd66f42d165526ce50e2c77a04432dff777e
Rainssong <rainssong@126.com>
2014-06-11 16:15:18 +0800

    Author

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
+//原作：wangqiuyun http://my.csdn.net/wangqiuyun
+//更新：rainssong http://blog.sina.com.cn/rainssong
 namespace IconBuilder
 {
     static class Program
